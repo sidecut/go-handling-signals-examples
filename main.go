2ca6fa2c8f8e25c23b83cbb405ee7bf8b8d4f233
James Raden <james.raden@gmail.com>
2023-02-10 13:51:25 -0500

    HTTP example

--- main.go
+++ main.go
@@ -1,23 +1,45 @@
-// From https://hjr265.me/blog/go-tidbit-handling-signals-exitting-gracefully/
-
 package main
 
 import (
+	"context"
 	"fmt"
+	"io"
+	"net/http"
 	"os"
 	"os/signal"
+	"time"
 )
 
 func main() {
-	fmt.Println("Waiting for signal.")
+	// Errors omitted for brevity.
 
-	// Make a buffered channel.
+	// Make an HTTP server.
+	server := http.Server{
+		Addr: ":8080",
+		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			// Simulate a slow HTTP response.
+			time.Sleep(10 * time.Second)
+			io.WriteString(w, "Hello")
+		}),
+	}
+
+	// Start the HTTP server in a separate Go routine.
+	go func() {
+		fmt.Println("Listening for HTTP connections.")
+		server.ListenAndServe()
+	}()
+
+	// Make a signal channel. Register SIGINT.
 	sigch := make(chan os.Signal, 1)
-	// Register the signals that you want to handle.
 	signal.Notify(sigch, os.Interrupt)
 
-	// Wait for the signal.
+	// Wait for signal.
 	<-sigch
 
-	fmt.Println("Received interrupt. Exiting.")
+	fmt.Println("Interrupted. Exiting.")
+
+	// Trigger a shutdown and allow 13 seconds to drain connections. Ignoring
+	// CancelFunc for brevity.
+	ctx, _ := context.WithTimeout(context.Background(), 13*time.Second)
+	server.Shutdown(ctx)
 }
